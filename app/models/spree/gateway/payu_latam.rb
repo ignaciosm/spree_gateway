308075cbe6b7d71cf31fbc1889606517dc254e77
ignaciosm <ignaciosm@gmail.com>
2017-06-16 11:51:33 -0500

    Update payu_latam.rb

--- app/models/spree/gateway/payu_latam.rb
+++ app/models/spree/gateway/payu_latam.rb
@@ -1,1 +1,16 @@
+module Spree
+  class Gateway::PayuLatamGateway < Gateway
+    preference :merchant_id, :string # ID
+    preference :account_id, :string # ID
+    preference :api_login, :string # ID
+    preference :api_key, :string # KEY
 
+    def provider_class
+      ActiveMerchant::Billing::PayuLatamGateway
+    end
+
+    def auto_capture?
+      true
+    end
+  end
+end
