783adfdacbd8ae9abf5e2956a929f675e1ae94a4
ignaciosm <ignaciosm@gmail.com>
2017-06-16 11:44:02 -0500

    Update culqi.rb

--- app/models/spree/gateway/culqi.rb
+++ app/models/spree/gateway/culqi.rb
@@ -1,1 +1,15 @@
+module Spree
+  class Gateway::CulqiGateway < Gateway
+    preference :merchant_id, :string # ID
+    preference :terminal_id, :string # ID
+    preference :secret_key, :string # KEY
 
+    def provider_class
+      ActiveMerchant::Billing::CulqiGateway
+    end
+
+    def auto_capture?
+      true
+    end
+  end
+end
